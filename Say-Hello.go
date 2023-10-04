3326a40fb4b24d04e6bea968f579b9c72dff62ff
ketzqt <squarpenz09@gmail.com>
2023-10-04 15:42:40 +0700

    update say hello with parameter name

--- Say-Hello.go
+++ Say-Hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Hello Ketz"
+func SayHello(name string) string {
+	return "Hello" + name
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/ketzqt/Go-say-hello
+module github.com/ketzqt/Go-say-hello/v2
 
 go 1.21.1
